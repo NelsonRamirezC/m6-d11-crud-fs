7e7f20bf32c526cf3f309815551b0d63e2e860b0
Nelson Ramirez <xhyskoax@gmail.com>
2024-05-20 21:03:40 -0400

    Se actualiza código con delete y update terminados

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.listen(3000, () => {
 });
 
 //MIDDLEWARES PARA PROCESAR DATOS DESDE EL CLIENTE AL SERVIDOR
+
 app.use(express.json()); //los guarda en req.body
 
 //públicar la carpeta public
@@ -76,21 +77,85 @@ app.post("/canciones", async(req, res) => {
             message: "Error al intentar agregar la nueva canción."
         });
     }
-
-
-    app.delete("/canciones", (req, res) => {
-        try {
-            res.json({
-                code: 200,
-                message: "Todo ok desde método delete"
-            })
-        } catch (error) {
-            res.status(500).json({
-                code: 500,
-                message: "Error al intentar eliminar la canción"
-            })
-        }
-    })
 });
 
+
+app.delete("/canciones", async (req, res) => {
+    try {
+        let { id } = req.query;
+
+        let cancionesJson = await fs.readFile(path.join(__dirname, "/listas/canciones.json"), "utf-8");
+
+        let listaCanciones = JSON.parse(cancionesJson);
+
+        let indiceCancion = listaCanciones.lista.findIndex(cancion => cancion.id == id);
+
+        if(indiceCancion >=0){
+          
+           listaCanciones.lista.splice(indiceCancion, 1);
+
+           await fs.writeFile(path.join(__dirname, "/listas/canciones.json"), JSON.stringify(listaCanciones, null, 4), "utf-8");
+
+            res.json({
+                code: 200,
+                message: "Todo ok desde método delete",
+                lista: listaCanciones.lista
+            })
+        }else {
+            res.status(404).json({
+                code: 404,
+                message: "No existe la canción que desea eliminar - ID: " + id 
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            code: 500,
+            message: "Error al intentar eliminar la canción"
+        })
+    }
+})
+
+
+
+//ENDPOINT PARA ACTUALIZAR CANCIONES
+
+app.put("/canciones", async(req, res) => {
+    try {
+
+      let {id, nombre} = req.body;
+
+        let cancionesJson = await fs.readFile(path.join(__dirname, "/listas/canciones.json"), "utf-8");
+
+        let listaCanciones = JSON.parse(cancionesJson);
+
+        let cancionFound = listaCanciones.lista.find(cancion => cancion.id == id);
+
+
+        if(cancionFound){
+
+            cancionFound.nombre = nombre;
+
+            await fs.writeFile(path.join(__dirname, "/listas/canciones.json"), JSON.stringify(listaCanciones, null, 4), "utf-8");
+
+        
+            res.json({
+                code: 201,
+                lista: listaCanciones.lista,
+                message: "Todo ok"
+            })
+
+        }else {
+            res.status(404).json({
+                code: 404,
+                message: "No existe la canción que desea actualizar - ID: " + id 
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            code: 500,
+            message: "Error al intentar agregar la nueva canción."
+        });
+    }
+});
 
